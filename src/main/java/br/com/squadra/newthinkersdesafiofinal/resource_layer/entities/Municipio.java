7383645c337dc1103fc3ba25544879b9b0d80d09
Junior Martins <juniorsoaresmartins@gmail.com>
2022-05-27 16:58:03 -0400

    Entity Município - Acrescimo do Pojo

---
+++ src/main/java/br/com/squadra/newthinkersdesafiofinal/resource_layer/entities/Municipio.java
@@ -0,0 +1,49 @@
+package br.com.squadra.newthinkersdesafiofinal.resource_layer.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_municipio")
+public final class Municipio implements Serializable {
+
+    // ---------- ATRIBUTOS DE CLASSE ---------- //
+    private static final Long serialVersionUID = 1L;
+
+    // ---------- ATRIBUTOS DE INSTÂNCIA ---------- //
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_municipio", nullable = false)
+    private Long codigoMunicipio;
+    @Column(name = "nome", length = 256, nullable = false)
+    private String nome;
+    @Column(name = "status", length = 3)
+    private Integer status;
+
+    // ---------- CONSTRUTORES ---------- //
+    public Municipio() {}
+
+    // ---------- MÉTODOS GETTERS E SETTERS ---------- //
+    public Long getCodigoMunicipio() {
+        return codigoMunicipio;
+    }
+
+    public void setCodigoMunicipio(Long codigoMunicipio) {
+        this.codigoMunicipio = codigoMunicipio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
