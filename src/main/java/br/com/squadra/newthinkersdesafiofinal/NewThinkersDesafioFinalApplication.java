c957c99d0d60dc34eb80200dae9d04cc1c7bce5b
Junior Martins <juniorsoaresmartins@gmail.com>
2022-05-27 18:19:43 -0400

    Configuração de ambientes múltiplos para Desenvolvimento e Produção

--- src/main/java/br/com/squadra/newthinkersdesafiofinal/NewThinkersDesafioFinalApplication.java
+++ src/main/java/br/com/squadra/newthinkersdesafiofinal/NewThinkersDesafioFinalApplication.java
@@ -1,13 +1,16 @@
 package br.com.squadra.newthinkersdesafiofinal;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewThinkersDesafioFinalApplication {
 
+	@Value("${application.name}")
+	private String applicationName;
+
 	public static void main(String[] args) {
 		SpringApplication.run(NewThinkersDesafioFinalApplication.class, args);
 	}
-
 }
--- src/main/java/br/com/squadra/newthinkersdesafiofinal/application_layer/controllers/PessoaController.java
+++ src/main/java/br/com/squadra/newthinkersdesafiofinal/application_layer/controllers/PessoaController.java
@@ -1,4 +1,14 @@
 package br.com.squadra.newthinkersdesafiofinal.application_layer.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PessoaController {
+
+    @PostMapping
+    public ResponseEntity<?> cadastrar() {
+        return null;
+    }
 }
