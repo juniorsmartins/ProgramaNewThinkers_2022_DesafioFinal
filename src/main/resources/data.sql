8d76420d1270c59f4a8dcd6b8dc4bdf03d9229a8
Junior Martins <juniorsoaresmartins@gmail.com>
2022-05-27 19:05:19 -0400

    Data.sql - inicialização de dados para testes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_pessoa(nome, sobrenome, idade, login, senha, status)
+    VALUES('Ludwig', 'Von Misses', 54, 'misses', 'misses123', 1);
+INSERT INTO tb_pessoa(nome, sobrenome, idade, login, senha, status)
+    VALUES('Olga', 'Weiss', 68, 'weiss', 'weiss123', 1);
+
+
+
+
+
+
