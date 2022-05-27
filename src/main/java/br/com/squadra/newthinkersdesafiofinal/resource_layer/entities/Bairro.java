bd9c1fbc147b50da673873baf24dad60c6d47ca4
Junior Martins <juniorsoaresmartins@gmail.com>
2022-05-27 17:03:48 -0400

    Entity Bairro - Acréscimo do Pojo

--- src/main/java/br/com/squadra/newthinkersdesafiofinal/resource_layer/entities/Bairro.java
+++ src/main/java/br/com/squadra/newthinkersdesafiofinal/resource_layer/entities/Bairro.java
@@ -4,31 +4,31 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "tb_municipio")
-public final class Municipio implements Serializable {
+@Table(name = "tb_bairro")
+public final class Bairro implements Serializable {
 
     // ---------- ATRIBUTOS DE CLASSE ---------- //
     private static final Long serialVersionUID = 1L;
 
     // ---------- ATRIBUTOS DE INSTÂNCIA ---------- //
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "codigo_municipio", nullable = false)
-    private Long codigoMunicipio;
+    @Column(name = "codigo_bairro", nullable = false)
+    private Long codigoBairro;
     @Column(name = "nome", length = 256, nullable = false)
     private String nome;
     @Column(name = "status", length = 3, nullable = false)
     private Integer status;
 
     // ---------- CONSTRUTORES ---------- //
-    public Municipio() {}
+    public Bairro() {}
 
     // ---------- MÉTODOS GETTERS E SETTERS ---------- //
-    public Long getCodigoMunicipio() {
-        return codigoMunicipio;
+    public Long getCodigoBairro() {
+        return codigoBairro;
     }
 
-    public void setCodigoMunicipio(Long codigoMunicipio) {
-        this.codigoMunicipio = codigoMunicipio;
+    public void setCodigoBairro(Long codigoBairro) {
+        this.codigoBairro = codigoBairro;
     }
 
     public String getNome() {
