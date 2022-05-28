6c898f33889310814991fee2b8a165a572d818ca
Junior Martins <juniorsoaresmartins@gmail.com>
2022-05-28 16:46:35 -0400

    Crud Municipio - Acréscimo de dados no Data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,6 +8,9 @@ INSERT INTO tb_uf(sigla, nome, status)
 INSERT INTO tb_uf(sigla, nome, status)
     VALUES('RS', 'Rio Grande do Sul', 1);
 
-
+INSERT INTO tb_municipio(nome, status)
+    VALUES('Cascavel', 1);
+INSERT INTO tb_municipio(nome, status)
+    VALUES('Porto Alegre', 1);
 
 
