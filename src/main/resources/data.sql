6d00b335424a868b34879b7fd3fb5e90d3e46a4f
Junior Martins <juniorsoaresmartins@gmail.com>
2022-05-28 14:52:09 -0400

    Crud Uf - Adição de informações de teste no Data.SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,10 @@ INSERT INTO tb_pessoa(nome, sobrenome, idade, login, senha, status)
 INSERT INTO tb_pessoa(nome, sobrenome, idade, login, senha, status)
     VALUES('Olga', 'Weiss', 68, 'weiss', 'weiss123', 1);
 
-
+INSERT INTO tb_uf(sigla, nome, status)
+    VALUES('PR', 'Paraná', 1);
+INSERT INTO tb_uf(sigla, nome, status)
+    VALUES('RS', 'Rio Grande do Sul', 1);
 
 
 
