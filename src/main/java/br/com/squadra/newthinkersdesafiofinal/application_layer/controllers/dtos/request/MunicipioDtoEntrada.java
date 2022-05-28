3f586e45683df93f932da1b754e5032ef2fa3f82
Junior Martins <juniorsoaresmartins@gmail.com>
2022-05-28 15:34:25 -0400

    Crud Município - criado Dto de entrada

--- src/main/java/br/com/squadra/newthinkersdesafiofinal/application_layer/controllers/dtos/request/MunicipioDtoEntrada.java
+++ src/main/java/br/com/squadra/newthinkersdesafiofinal/application_layer/controllers/dtos/request/MunicipioDtoEntrada.java
@@ -1,4 +1,24 @@
 package br.com.squadra.newthinkersdesafiofinal.application_layer.controllers.dtos.request;
 
-public class MunicipioDtoEntrada {
+import org.hibernate.validator.constraints.Length;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public final class MunicipioDtoEntrada {
+
+    // ---------- ATRIBUTOS DE INSTÂNCIA ---------- //
+    @NotNull @NotEmpty @Length(max = 256)
+    private String nome;
+
+    // ---------- CONSTRUTORES ---------- //
+    public MunicipioDtoEntrada() {}
+
+    // ---------- MÉTODOS GETTERS E SETTERS ---------- //
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
