29324a460cc72153b8e7c6efad3cdf5a0d6a50b7
Junior Martins <juniorsoaresmartins@gmail.com>
2022-05-28 13:59:51 -0400

    Crud Uf - Construído DTO de Entrada

--- src/main/java/br/com/squadra/newthinkersdesafiofinal/application_layer/controllers/dtos/request/UfDtoEntrada.java
+++ src/main/java/br/com/squadra/newthinkersdesafiofinal/application_layer/controllers/dtos/request/UfDtoEntrada.java
@@ -1,4 +1,35 @@
 package br.com.squadra.newthinkersdesafiofinal.application_layer.controllers.dtos.request;
 
-public class UfDtoEntrada {
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public final class UfDtoEntrada {
+
+    // ---------- ATRIBUTOS DE INSTÂNCIA ---------- //
+    @NotNull @NotEmpty @Length(max = 2)
+    private String sigla;
+    @NotNull @NotEmpty @Length(max = 60)
+    private String nome;
+
+    // ---------- CONSTRUTORES ---------- //
+    public UfDtoEntrada() {}
+
+    // ---------- MÉTODOS GETTERS E SETTERS ---------- //
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
