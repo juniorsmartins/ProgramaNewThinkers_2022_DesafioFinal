3b0ccdcd4e04524256b47e338e3505acfb5a7d97
Junior Martins <juniorsoaresmartins@gmail.com>
2022-05-29 06:59:40 -0400

    Crud bairro - Criado DTO de saída

--- src/main/java/br/com/squadra/newthinkersdesafiofinal/application_layer/controllers/dtos/response/BairroDtoSaida.java
+++ src/main/java/br/com/squadra/newthinkersdesafiofinal/application_layer/controllers/dtos/response/BairroDtoSaida.java
@@ -1,4 +1,44 @@
 package br.com.squadra.newthinkersdesafiofinal.application_layer.controllers.dtos.response;
 
-public class BairroDtoSaida {
+import br.com.squadra.newthinkersdesafiofinal.resource_layer.entities.Bairro;
+
+public final class BairroDtoSaida {
+
+    // ---------- ATRIBUTOS DE INSTÂNCIA ---------- //
+    private Long codigoBairro;
+    private String nome;
+    private Integer status;
+
+    // ---------- CONSTRUTORES ---------- //
+    public BairroDtoSaida() {}
+    public BairroDtoSaida(Bairro bairro) {
+        setCodigoBairro(bairro.getCodigoBairro());
+        setNome(bairro.getNome());
+        setStatus(bairro.getStatus());
+    }
+
+    // ---------- MÉTODOS GETTERS E SETTERS ---------- //
+    public Long getCodigoBairro() {
+        return codigoBairro;
+    }
+
+    public void setCodigoBairro(Long codigoBairro) {
+        this.codigoBairro = codigoBairro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 }
