84d0f0b6ee9c0a043118542700019a1ea7b54ae6
Junior Martins <juniorsoaresmartins@gmail.com>
2022-05-29 07:04:02 -0400

    Crud Bairro - Criado DTO de entrada

--- src/main/java/br/com/squadra/newthinkersdesafiofinal/application_layer/controllers/dtos/request/BairroDtoEntrada.java
+++ src/main/java/br/com/squadra/newthinkersdesafiofinal/application_layer/controllers/dtos/request/BairroDtoEntrada.java
@@ -1,4 +1,24 @@
 package br.com.squadra.newthinkersdesafiofinal.application_layer.controllers.dtos.request;
 
-public class BairroDtoEntrada {
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
+public final class BairroDtoEntrada {
+
+    // ---------- ATRIBUTOS DE INSTÂNCIA ---------- //
+    @NotBlank @Length(max = 256)
+    private String nome;
+
+    // ---------- CONSTRUTORES ---------- //
+    public BairroDtoEntrada() {}
+
+    // ---------- MÉTODOS GETTERS E SETTERS ---------- //
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
