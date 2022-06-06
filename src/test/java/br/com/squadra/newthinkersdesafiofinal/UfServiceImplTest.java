0195ccb408b40af06c09dd030e3fc4b32ad87c3f
Junior Martins <juniorsoaresmartins@gmail.com>
2022-06-06 09:51:45 -0400

    feat-database-oracle - início de testes unitários - na branch errada

---
+++ src/test/java/br/com/squadra/newthinkersdesafiofinal/UfServiceImplTest.java
@@ -0,0 +1,19 @@
+package br.com.squadra.newthinkersdesafiofinal;
+
+import br.com.squadra.newthinkersdesafiofinal.domain_layer.services.UfServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class UfServiceImplTest {
+
+    @Autowired
+    private UfServiceImpl ufService;
+
+    @Test
+    public void testeUfService() {
+
+    }
+}
