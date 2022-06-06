5c193ce888e60cac3484ebe5a45cfc976f70f385
Junior Martins <juniorsoaresmartins@gmail.com>
2022-06-06 07:22:36 -0400

    feat-database-oracle - retorno ao ambiente de development

--- src/main/java/br/com/squadra/newthinkersdesafiofinal/NewThinkersDesafioFinalApplication.java
+++ src/main/java/br/com/squadra/newthinkersdesafiofinal/NewThinkersDesafioFinalApplication.java
@@ -3,10 +3,6 @@ package br.com.squadra.newthinkersdesafiofinal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class NewThinkersDesafioFinalApplication {
